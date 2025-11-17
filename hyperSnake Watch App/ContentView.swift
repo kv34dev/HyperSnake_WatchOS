b104b92c585e937cba1f605480afdfcbca8949bd
Vladimir Kudryashov <doctorinternet@icloud.com>
2025-11-17 16:18:05 +0100

    Update ContentView.swift

--- hyperSnake Watch App/ContentView.swift
+++ hyperSnake Watch App/ContentView.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var color = Color.green
+    @State private var speed = 7
+    
     var body: some View {
-        SnakeGameView()
+        NavigationStack {
+            ColorSelectionView(selectedColor: $color,
+                               selectedSpeed: $speed)
+        }
     }
 }
