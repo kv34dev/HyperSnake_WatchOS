3004bb3a4054a7a46e0ebcbf6728f1c5f4156abe
Vladimir Kudryashov <doctorinternet@icloud.com>
2025-11-18 17:38:37 +0100

    Update DifficultyView.swift

--- hyperSnake Watch App/DifficultyView.swift
+++ hyperSnake Watch App/DifficultyView.swift
@@ -7,59 +7,60 @@ struct DifficultyView: View {
     @State private var goToGame = false
     
     let difficulties: [(String, Int)] = [
-        //("Very Easy", 1), //test
         ("Easy", 2),
         ("Normal", 4),
         ("Hard", 7)
     ]
     
     var body: some View {
-        VStack(spacing: 12) {
-            
-            // Карточка сложности
-            VStack(alignment: .leading, spacing: 12) {
-                Text("Difficulty")
-                    .font(.system(size: 18, weight: .bold))
-                    .foregroundStyle(.white)
+        ScrollView { // прокрутка
+            VStack(spacing: 12) {
                 
-                ForEach(difficulties, id: \.0) { name, speed in
-                    VStack(alignment: .leading, spacing: 2) {
-                        Text(name)
-                            .font(.system(size: 18))
-                            .foregroundColor(.white)
-                        
-                        if speed == selectedSpeed {
-                            Rectangle()
-                                .fill(Color.white)
-                                .frame(height: 3)
-                                .cornerRadius(2)
-                                .padding(.leading, 2)
-                                .frame(width: 80, alignment: .leading)
+                // Карточка сложности
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Difficulty")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundStyle(.white)
+                    
+                    ForEach(difficulties, id: \.0) { name, speed in
+                        VStack(alignment: .leading, spacing: 2) {
+                            Text(name)
+                                .font(.system(size: 18))
+                                .foregroundColor(.white)
+                            
+                            if speed == selectedSpeed {
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .frame(height: 3)
+                                    .cornerRadius(2)
+                                    .padding(.leading, 2)
+                                    .frame(width: 80, alignment: .leading)
+                            }
                         }
+                        .onTapGesture { selectedSpeed = speed }
                     }
-                    .onTapGesture { selectedSpeed = speed }
                 }
+                .padding(12)
+                .background(Color(red: 0.12, green: 0.12, blue: 0.16))
+                .cornerRadius(14)
+                
+                NavigationLink(
+                    destination:
+                        GameView(snakeColor: selectedColor,
+                                 speed: selectedSpeed),
+                    isActive: $goToGame
+                ) { EmptyView() }
+                
+                Button(action: { goToGame = true }) {
+                    Text("START")
+                        .font(.system(size: 22, weight: .bold))
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(Color.blue)
             }
-            .padding(12)
-            .background(Color(red: 0.12, green: 0.12, blue: 0.16))
-            .cornerRadius(14)
-            
-            NavigationLink(
-                destination:
-                    GameView(snakeColor: selectedColor,
-                             speed: selectedSpeed),
-                isActive: $goToGame
-            ) { EmptyView() }
-            
-//            Button(action: { goToGame = true }) {
-//                Text("START")
-//                    .font(.system(size: 22, weight: .bold))
-//                    .frame(maxWidth: .infinity)
-//            }
-//            .buttonStyle(.borderedProminent)
-//            .tint(Color.blue)
+            .padding()
         }
-        .padding()
         .background(Color(red: 0.05, green: 0.05, blue: 0.07))
     }
 }
